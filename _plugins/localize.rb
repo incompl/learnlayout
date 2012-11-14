dd33a185a5f1a751e66317a30fcd8d283c0e70f5
Greg Smith <greg@bocoup.com>
2012-11-14 18:17:32 -0500

    internationalization plugin

---
+++ _plugins/localize.rb
@@ -0,0 +1,31 @@
+module Jekyll
+  class LocalizeTag < Liquid::Tag
+
+    def initialize(tag_name, key, tokens)
+      super
+      @init = false
+      @key = key.strip
+    end
+
+    def render(context)
+      @lang = context.registers[:site].config['lang']
+      @translations = YAML::load(File.open("translations/#{@lang}.yaml"))
+      @init = true
+
+      if @key[0..3] == 'page'
+        tokens = @key.split('.')
+        @key = context.environments.first
+        while tokens.length > 0
+          token = tokens.shift
+          @key = @key[token]
+        end
+      end
+      
+      result = @translations[@key]
+
+      "#{result}"
+    end
+  end
+end
+
+Liquid::Template.register_tag('localize', Jekyll::LocalizeTag)
